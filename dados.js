413f48ab7c5ec35ef95509bf03a2a35e5862065e
gustavoodev10 <gugasartori10@gmail.com>
2025-05-20 10:10:10 -0300

    ADiciona TUDO

---
+++ dados.js
@@ -0,0 +1,18 @@
+{
+    "clientes"[
+        {
+            "id": 1,
+            "cpf": "111.111.111-11",
+            "nome": "Gustavo Sartori",
+            "telefone": "(18)9999-9999",
+            "cidade": "Presidente Prudente"
+        },
+        {
+            "id": 2,
+            "cpf": "222.222.222-22",
+            "nome": "Bia Sartori",
+            "telefone": "(18)11111-1111",
+            "cidade": "Dracena"
+        }
+    ]
+}
